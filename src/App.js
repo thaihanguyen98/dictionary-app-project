22170bacf32cf381d34ca59853cde4349e010b9a
thaihanguyen98 <142948159+thaihanguyen98@users.noreply.github.com>
2023-11-14 19:18:04 +1100

    misspell definitions in Meaning component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import React from "react";
 
 function App() {
   return (
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definition.map(function (definition, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
               {definition.definition}
               <br />
-              <em>{definition.example}</em>{" "}
+              <em>{definition.example}</em>
             </p>
           </div>
         );
