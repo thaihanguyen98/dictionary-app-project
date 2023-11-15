3d4be78c44b5dad03ffb86a2bbf984373eb9231a
thaihanguyen98 <142948159+thaihanguyen98@users.noreply.github.com>
2023-11-15 15:52:48 +1100

    Add the synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        <ul>
+          {props.synonyms.map(function (synonyms, index) {
+            return <li key={index}>{synonyms}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
